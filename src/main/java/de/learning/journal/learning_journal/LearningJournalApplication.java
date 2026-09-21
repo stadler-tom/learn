137beeb116899f29f9b9e9bedf00b0752cdcde16
stadler-tom <stadler.tom@googlemail.com>
2026-09-21 10:53:51 +0200

    feat: adding maven and spring-boot to the project (#2)
    * feat: adding maven and spring-boot to the project
    
    * fix: adding missing tags im pom.xml. url stays emtpy no url but localhost
    
    ---------
    
    Co-authored-by: stadl_th <thomas-a.stadler@rohde-schwarz.com>

---
+++ src/main/java/de/learning/journal/learning_journal/LearningJournalApplication.java
@@ -0,0 +1,13 @@
+package de.learning.journal.learning_journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningJournalApplication.class, args);
+	}
+
+}
