2f7758e871b8d54a936f65640e07f02e743f06e5
stadl_th <thomas-a.stadler@rohde-schwarz.com>
2026-09-21 10:35:31 +0200

    feat: adding maven and spring-boot to the project

---
+++ src/test/java/de/learning/journal/learning_journal/LearningJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package de.learning.journal.learning_journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
